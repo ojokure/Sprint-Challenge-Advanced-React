cfab550b9797f50d3fc5547f7a151cbb92c60088
oladimeji-ojo <ojokuredim@gmail.com>
2019-10-04 09:18:48 +0100

    created a class component Players and passed data to it from App as props

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,26 @@
 import React from "react";
 import "./App.css";
 import axios from "axios";
+import Players from "./Players";
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { api : [ ] };
+    this.state = { api: [] };
   }
 
-
-  componentDidMount(){
-      axios.get('http://localhost:5000/api/players')
-      .then(res => {
-        this.setState({api: res.data})
-      })
+  componentDidMount() {
+    axios.get("http://localhost:5000/api/players").then(res => {
+      this.setState({ api: res.data });
+    });
   }
-
 
   render() {
     return (
       <div className="App">
-         
+        <Players players={this.state.users} />
       </div>
     );
   }
---
+++ client/src/Players.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default class Players extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+  render() {
+    const { players } = this.props;
+    return (
+    <div>
+
+
+
+
+
+    </div>);
+  }
+}
