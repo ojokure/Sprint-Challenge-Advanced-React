1ce311b2de0cc93c17034e9c7dc7a95a4453ce03
oladimeji-ojo <ojokuredim@gmail.com>
2019-10-04 09:40:37 +0100

    converted class component players to a function component

--- client/src/Players.js
+++ client/src/Players.js
@@ -1,21 +1,19 @@
 import React from "react";
 
-export default class Players extends React.Component {
-  //   constructor(props) {
-  //     super(props);
-  //   }
-  render() {
-    const { players } = this.props;
-    return (
-      <div>
-       {players.map(el => (
-            <div>
-              {el.name}
-                <p>{el.country}</p>
-               <p>{el.searches}</p>
-            </div>
-        ))}
-      </div>
-    );
-  }
+export default function PLayers(props) {
+  const { players } = props;
+
+
+
+  return (
+    <div>
+      {players.map(el => (
+        <div>
+          {el.name}
+          <p>{el.country}</p>
+          <p>{el.searches}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
